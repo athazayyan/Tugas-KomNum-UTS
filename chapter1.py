4aee86012e44892fee0293ba7adb4a2271f3b12c
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-10-20 19:08:26 +0700

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -64,25 +64,8 @@ else:
 
 '...Selesai..'
 
-x_range = [-10, 10]  
-plot_formula(fx1, x_range)
 
 
-# Fungsi untuk plotting rumus matematika
-def plot_formula(formula, x_range):
-    # Mengubah rumus menjadi fungsi python menggunakan eval
-    x = np.linspace(x_range[0], x_range[1], 1000)
-    y = eval(formula)
-    
-    # Plotting hasilnya
-    plt.figure(figsize=(8, 6))
-    plt.plot(x, y, label=f"y = {formula}")
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title(f"Plot of y = {formula}")
-    plt.grid(True)
-    plt.legend()
-    plt.show()
 
 
 
